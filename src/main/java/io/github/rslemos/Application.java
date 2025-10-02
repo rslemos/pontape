f7acab72bd6902231cf002a59f90d7b3b8a9417a
rslemos <rslemos@gmail.com>
2025-10-01 22:05:51 -0300

    wip

---
+++ src/main/java/io/github/rslemos/Application.java
@@ -0,0 +1,11 @@
+package io.github.rslemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(io.github.rslemos.Application.class, args);
+    }
+}
